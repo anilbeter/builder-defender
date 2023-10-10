381cea0d2e831b173a0f0e1260fdaf7bea5264d1
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-10-10 17:01:39 +0300

    edit ResourceTypeSO script

--- Assets/Scripts/ResourceTypeSO.cs
+++ Assets/Scripts/ResourceTypeSO.cs
@@ -2,17 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResourceTypeSO : MonoBehaviour
+[CreateAssetMenu(menuName = "ScriptableObjects/ResourceType")]
+public class ResourceTypeSO : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string nameString;
 }
