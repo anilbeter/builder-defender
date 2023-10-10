c8dd398f867d5c49a6f6e22b834dab86e4344716
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-10-10 17:26:05 +0300

    setup ResourceManager script

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Dictionary<ResourceTypeSO, int> resourceAmountDictionary;
+
+    private void Awake()
     {
-        
+        resourceAmountDictionary = new Dictionary<ResourceTypeSO, int>();
+        ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+
+        foreach (ResourceTypeSO resourceType in resourceTypeList.list)
+        {
+            resourceAmountDictionary[resourceType] = 0;
+        }
+        TestLogResourceAmountDictionary();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.T)) 
+        {
+            ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+            AddResource(resourceTypeList.list[0], 2);
+            TestLogResourceAmountDictionary();
+        }
+    }
+
+    public void AddResource(ResourceTypeSO resourceType, int amount)
+    {
+        resourceAmountDictionary[resourceType] += amount;
+    }
+    private void TestLogResourceAmountDictionary()
+    {
+        foreach (ResourceTypeSO resourceType in resourceAmountDictionary.Keys)
+        {
+            Debug.Log(resourceType.nameString + ": " + resourceAmountDictionary[resourceType]);
+        }
     }
 }
+
